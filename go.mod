caeb6f192e524c15c37b25303cfbaa66808601cb
prathyushnallamothu <prathyushbablu@gmail.com>
2020-07-12 22:51:16 +0530

    second commit

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/prathyushnallamothu/project1
 
 go 1.14
+
+require github.com/gorilla/mux v1.7.4
--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
+import (
+	"html/template"
+	"net/http"
 
-import(
-	"fmt"
+	"github.com/gorilla/mux"
 )
-
+var tpl *template.Template
+func init(){
+	tpl=template.Must(template.ParseGlob("templates/*.html"))
+}
 func main(){
-	fmt.Println("hi i am learning git")
+	m:=mux.NewRouter().StrictSlash(true)
+	m.HandleFunc("/",homehandler)
+	http.ListenAndServe(":8080",m)
+}
+func homehandler(w http.ResponseWriter,r *http.Request){
+	tpl.ExecuteTemplate(w,"home.html",nil)
 }
