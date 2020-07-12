552fb0feb68509ac399fb57ec6f2c3c36324c772
prathyushnallamothu <prathyushbablu@gmail.com>
2020-07-12 23:18:07 +0530

    fourth commit

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/prathyushnallamothu/project1
 
 go 1.14
 
-require github.com/gorilla/mux v1.7.4
+require (
+	github.com/gorilla/mux v1.7.4
+	github.com/joho/godotenv v1.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,21 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	"github.com/prathyushnallamothu/project1/structs"
 )
 
 var tpl *template.Template
-
+var x string
 func init() {
+	err:=godotenv.Load("./.env")
+	if err!=nil{
+		log.Fatal(err)
+	}
+	x=os.Getenv("MY_FAV_HERO")
 	tpl = template.Must(template.ParseGlob("templates/*.html"))
 }
 func main() {
 	m := mux.NewRouter().StrictSlash(true)
 	m.HandleFunc("/", homehandler)
+	fmt.Println(x)
 	http.ListenAndServe(":8080", m)
 }
 func homehandler(w http.ResponseWriter, r *http.Request) {
